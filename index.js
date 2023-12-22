571b820f684a3dc04c121a15d3c5447d1356e0de
Shivam Mishra <shivam552mishra@gmail.com>
2023-12-22 16:48:49 +0530

    analogClock added

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hRotation = 30*htime + mtime/2;
+    mRotation = 6*mtime;
+    sRotation = 6*stime;
+
+    hr.style.transform = `rotate(${hRotation}deg)`
+    min.style.transform = `rotate(${mRotation}deg)`
+    sec.style.transform = `rotate(${sRotation}deg)`
+
+},1000);
